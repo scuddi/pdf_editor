7156766029d99f991a4616988a56937a87aafa38
scuddi <157764402+scuddi@users.noreply.github.com>
2024-04-08 15:44:40 +0200

    finished "Startseite" with info text; next step would be to write the functions behind the segmented buttons

--- pdf_editor_gui.py
+++ pdf_editor_gui.py
@@ -10,16 +10,32 @@ app = customtkinter.CTk()
 app.geometry("600x500")
 app.title("PDF Editor by KS")
 
+# main function for buttons to load content into main_frame
+
+"""def main_function(value):"""
+
 # navigation bar / tab bar
 
-tabs_button = customtkinter.CTkSegmentedButton(master = app, values = ["Startseite", "PDF-Merger", "PDF-Snipping", "PDF-Converter"], width = 580)
+tabs_button = customtkinter.CTkSegmentedButton(master = app, values = ["Startseite", "PDF-Merger", "PDF-Snipping", "PDF-Converter"],
+                                               width = 580,
+                                               #command = main_function)
 tabs_button.set("Startseite")
+
+
+main_frame = customtkinter.CTkFrame(master=app, width = 580, height = 430)
 
 # Startseite - Hinweis
 
-frame1 = customtkinter.CTkFrame(master=app, fg_color="#ad3f37", border_color="#240d0c", border_width=2, width = 580)
 
-#hinweis = customtkinter.CTkLabel(master)
+hinweis = customtkinter.CTkLabel(master = main_frame,
+                                 width = 560,
+                                 height = 410,
+                                 text = "Wilkommen in meinem PDF-Editor\n\n"
+                                        "Hier entstehen verschiedene Funktionen eines PDF-Editors.\n\n"
+                                        "Die verschiedenen Funktionen sind/werden sein: \n\n"
+                                        "    - PDF-Merger zum zusammenführen von zwei PDFs\n"
+                                        "    - PDF-Snipping zum entfernen einzelner Seiten\n"
+                                        "    - PDF-Converter zum transformieren von Word zu PDFs und anders herum")
 
 
 # define grid
@@ -30,6 +46,7 @@ app.rowconfigure((0,1), weight = 0)
 # grid for gui layout
 
 tabs_button.grid(row = 0, column = 0, columnspan = 2, padx = 10, pady= 10, sticky = "new")
-frame1.grid(row = 1, column = 0, columnspan = 2, padx = 10, sticky = "ew")
+main_frame.grid(row = 1, column = 0, columnspan = 2, padx = 10, sticky = "ew")
+hinweis.grid(row = 1, column = 0, columnspan = 2, padx = 10, pady = 15, sticky = "nsew")
 
 app.mainloop()
