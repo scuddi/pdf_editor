50581da71d11b37e27ee2db10e09fec6350117f1
scuddi <157764402+scuddi@users.noreply.github.com>
2024-04-18 15:58:58 +0200

    trying to build the function for the button to switch frames; currently unsure how to do

--- pdf_editor_gui.py
+++ pdf_editor_gui.py
@@ -12,23 +12,31 @@ app.title("PDF Editor by KS")
 
 # main function for buttons to load content into main_frame
 
-"""def main_function(value):"""
+tab_values = ["Startseite", "PDF-Merger", "PDF-Snipping", "PDF-Converter"]
+
+main_frame = tab_values
+
+# main function for buttons to load content into main_frame
+def tab_click(button_value):
+    print(button_value)
+    if button_value == "Startseite":
+        return main_frame == starting_frame
+    elif button_value == "PDF-Merger":
+        return main_frame == merger_frame
 
 # navigation bar / tab bar
 
-tabs_button = customtkinter.CTkSegmentedButton(master = app, values = ["Startseite", "PDF-Merger", "PDF-Snipping", "PDF-Converter"],
+tabs_button = customtkinter.CTkSegmentedButton(master = app, values = tab_values,
                                                width = 580,
-                                               #command = main_function
+                                               command = tab_click
                                                )
 tabs_button.set("Startseite")
 
-
-main_frame = customtkinter.CTkFrame(master = app, width = 580, height = 430)
-
 # Startseite - Hinweis
 
+starting_frame = customtkinter.CTkFrame(master = app, width = 580, height = 430)
 
-hinweis = customtkinter.CTkLabel(master = main_frame,
+hinweis = customtkinter.CTkLabel(master = starting_frame,
                                  width = 560,
                                  height = 410,
                                  text = "Wilkommen in meinem PDF-Editor\n\n"
@@ -37,6 +45,10 @@ hinweis = customtkinter.CTkLabel(master = main_frame,
                                         "    - PDF-Merger zum zusammenführen von zwei PDFs\n"
                                         "    - PDF-Snipping zum entfernen einzelner Seiten\n"
                                         "    - PDF-Converter zum transformieren von Word zu PDFs und anders herum")
+
+# merger tab
+
+merger_frame = customtkinter.CTkFrame(master = app, width = 580, height = 430)
 
 
 # define grid
@@ -47,7 +59,8 @@ app.rowconfigure((0,1), weight = 0)
 # grid for gui layout
 
 tabs_button.grid(row = 0, column = 0, columnspan = 2, padx = 10, pady= 10, sticky = "new")
-main_frame.grid(row = 1, column = 0, columnspan = 2, padx = 10, sticky = "ew")
+starting_frame.grid(row = 1, column = 0, columnspan = 2, padx = 10, sticky = "ew")
 hinweis.grid(row = 1, column = 0, columnspan = 2, padx = 10, pady = 15, sticky = "nsew")
 
+# merger_frame.grid(row = 1, column = 0, columnspan = 2, padx = 10, sticky = "ew")
 app.mainloop()
