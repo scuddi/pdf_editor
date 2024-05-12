38fc4e4dd6f034895d3f1b1c3d47ae1b73d397ae
scuddi <157764402+scuddi@users.noreply.github.com>
2024-05-12 17:38:45 +0200

    switching frames with a click on the "Tabs"-Buttons now works; added the "filler" messages for the frames as well; next step: fill it with functional content, starting off with the already written pdf merger

--- pdf_editor_gui.py
+++ pdf_editor_gui.py
@@ -16,23 +16,40 @@ tab_values = ["Startseite", "PDF-Merger", "PDF-Snipping", "PDF-Converter"]
 
 # main function for buttons to load content into main_frame
 
-current_frame = None
-starting_frame = None
-merger_frame = None
-
 def tab_click(button_value):
     print(button_value)
     if button_value == "Startseite":
-        current_frame = starting_frame_set()
+        starting_frame_set()
     elif button_value == "PDF-Merger":
-        current_frame = pdf_merger_frame()
+        pdf_merger_frame()
+    elif button_value == "PDF-Snipping":
+        pdf_snipping_frame()
+    elif button_value == "PDF-Converter":
+        pdf_converter_frame()
 
 def starting_frame_set():
     starting_frame.grid(row = 1, column = 0, columnspan = 2, padx = 10, sticky = "ew")
+    merger_frame.grid_forget()
+    snipping_frame.grid_forget()
+    converter_frame.grid_forget()
 
 def pdf_merger_frame():
     merger_frame.grid(row = 1, column = 0, columnspan = 2, padx = 10, sticky = "ew")
+    starting_frame.grid_forget()
+    snipping_frame.grid_forget()
+    converter_frame.grid_forget()
 
+def pdf_snipping_frame():
+    snipping_frame.grid(row = 1, column = 0, columnspan = 2, padx = 10, sticky = "ew")
+    starting_frame.grid_forget()
+    merger_frame.grid_forget()
+    converter_frame.grid_forget()
+
+def pdf_converter_frame():
+    converter_frame.grid(row = 1, column = 0, columnspan = 2, padx = 10, sticky = "ew")
+    starting_frame.grid_forget()
+    merger_frame.grid_forget()
+    snipping_frame.grid_forget()
 
 # navigation bar / tab bar
 
@@ -67,6 +84,26 @@ merger_message = customtkinter.CTkLabel(master = merger_frame,
                                         text = "Hier können PDFs zusammengefügt werden."
                                         )
 
+# snipping frame
+
+snipping_frame = customtkinter.CTkFrame(master = app, width = 580, height = 430)
+
+snipping_message = customtkinter.CTkLabel(master = snipping_frame,
+                                          width = 560,
+                                          height = 200,
+                                          text = "Hier können einzelne Seiten aus PDFs entfernt werden."
+                                          )
+
+# converter frame
+
+converter_frame = customtkinter.CTkFrame(master = app, width = 580, height = 430)
+
+converter_message = customtkinter.CTkLabel(master = converter_frame,
+                                           width = 560,
+                                           height = 200,
+                                           text = "Hier können Sie (Word-) Dateien in PDFs umwandeln."
+                                           )
+
 # define grid
 
 app.columnconfigure((0,1), weight = 0)
@@ -77,7 +114,8 @@ app.rowconfigure((0,1), weight = 0)
 tabs_button.grid(row = 0, column = 0, columnspan = 2, padx = 10, pady= 10, sticky = "new")
 starting_frame.grid(row = 1, column = 0, columnspan = 2, padx = 10, sticky = "ew")
 hinweis.grid(row = 1, column = 0, columnspan = 2, padx = 10, pady = 15, sticky = "nsew")
-#merger_frame.grid(row = 1, column = 0, columnspan = 2, padx = 10, sticky = "ew")
 merger_message.grid(row = 1, column = 0, columnspan = 2, padx = 10, pady = 15, sticky = "nsew")
+snipping_message.grid(row = 1, column = 0, columnspan = 2, padx = 10, pady = 15, sticky = "nsew")
+converter_message.grid(row = 1, column = 0, columnspan = 2, padx = 10, pady = 15, sticky = "nsew")
 
 app.mainloop()
